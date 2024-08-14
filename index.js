70de9f37bda05d20a64662211c1a4c805f15c827
okeStevo <134113598+okeStevo@users.noreply.github.com>
2024-08-14 18:13:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,30 +5,40 @@ const checkboxErrorMessage = document.getElementById("checkbox-error");
 const checkBox = document.getElementById("terms-and-condition");
 console.log(errorPassword);
 formElement.addEventListener("submit", function (event) {
-    errorPassword.textContent = ""
-    errorUsername.textContent = ""
-    event.preventDefault()
-    const formData = new FormData(event.target)
-    const userName = formData.get("username")
-    const password = formData.get("password")
-    if (!userName || !password) {
-        if (!password && userName) {
-            errorPassword.style.color = "red"
-            errorPassword.textContent = "please enter a valid password!"
-            return
-        }
-        if (!userName && password) {
-            errorUsername.style.color = "red"
-            errorUsername.textContent = "please enter a valid username! "
-            return
-        }
-        
-        return
+  errorPassword.textContent = "";
+  errorUsername.textContent = "";
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const userName = formData.get("username");
+  const password = formData.get("password");
+  errorUsername.style.color = "red";
+  errorPassword.style.color = "red";
+  if (!userName || !password) {
+    if (!userName) {
+      errorUsername.innerHTML = "please enter a  username! ";
+      return;
     }
-})
+    if (!password) {
+      errorPassword.textContent = "please entere a password";
+      return;
+    }
 
-
-
-
-
-// some changessss
+    errorUsername.textContent = "please enter a valid username! ";
+    errorPassword.textContent = "please enter a valid password!";
+    return;
+  }
+  if (!checkBox.checked) {
+    event.preventDefault(); // Prevent form submission
+    checkboxErrorMessage.innerHTML =
+      "You must agree to <br/> the terms and conditions.";
+    checkboxErrorMessage.style.display = "block";
+    return;
+  } else {
+    checkboxErrorMessage.textContent = "";
+  }
+  let popup = document.getElementById("popup");
+  popup.classList.add("show");
+  setTimeout(function () {
+    popup.classList.remove("show");
+  }, 5000);
+});
